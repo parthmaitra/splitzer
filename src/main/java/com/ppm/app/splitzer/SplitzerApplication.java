f35b702280f7b1e4e55b5843fa02d28365aad1ce
parthmaitra <maitraparth@gmail.com>
2024-08-17 22:55:12 +0530

    Created base project from Initalizr

---
+++ src/main/java/com/ppm/app/splitzer/SplitzerApplication.java
@@ -0,0 +1,13 @@
+package com.ppm.app.splitzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ppm/app/splitzer/SplitzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppm.app.splitzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
